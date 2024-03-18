2e48e108c41daf7b5fc9f4c66dece89634508be4
mehediemon007 <mehediemon7@gmail.com>
2024-03-19 01:00:03 +0600

    faq

--- app/elements/Accordion.jsx
+++ app/elements/Accordion.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
-const Accordion = () => {
+const Accordion = ({ques, ans}) => {
 
     const [accordionOpen, setAccordionOpen] = useState(false)
 
@@ -19,7 +19,7 @@ const Accordion = () => {
                         onClick={(e) => { e.preventDefault(); setAccordionOpen(!accordionOpen); }}
                         aria-expanded={accordionOpen}
                         aria-controls={`accordion-text-01`}>
-                        <span>What are the costs associated with a Prism Card?</span>
+                        <span>{ques}</span>
                         <svg className="fill-indigo-500 shrink-0 ml-8" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                             <rect y="7" width="16" height="2" rx="1" className={`ttransform origin-center transition duration-200 ease-out ${accordionOpen && '!rotate-180'}`} />
                             <rect y="7" width="16" height="2" rx="1" className={`transform origin-center rotate-90 transition duration-200 ease-out ${accordionOpen && '!rotate-180'}`} />
@@ -30,9 +30,7 @@ const Accordion = () => {
                     aria-labelledby={`accordion-title-01`}
                     className={`grid text-sm text-slate-600 overflow-hidden transition-all duration-300 ease-in-out ${accordionOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}>
                     <div className="overflow-hidden">
-                        <p className="pb-3">
-                            If you go over your organisations or user limit, a member of the team will reach out about bespoke pricing. In the meantime, our collaborative features won't appear in accounts or users that are over the 100-account or 1,000-user limit.
-                        </p>
+                        <p className="pb-3">{ans}</p>
                     </div>
                 </div>
             </div>
