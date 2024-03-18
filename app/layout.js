a7a14f8773a48bc5a0e5cb8c046a9d1b8abe87e7
mehediemon007 <mehediemon7@gmail.com>
2024-03-18 12:40:57 +0600

    next set-up

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+
+export const metadata = {
+  title: "Prism",
+  description: "Developed by Mehedi Hasan Emon, mail: mehediemon7@gmail.com",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
