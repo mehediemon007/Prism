552ec496d6033c5d0508bb75f0ffb55d63484e5c
mehediemon007 <mehediemon7@gmail.com>
2024-03-19 20:16:12 +0600

    fonts

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
                         <h2 className='text-[40px]/[50px] font-title font-bold tracking-wide'>Global Spending with Prism Card</h2>
                         <p className='text-2xl/[40px] font-title font-bold tracking-[1px] mt-6'>Your Prism Card lets you freely spend your cryptocurrency at any location and time across the globe, providing a seamless way to use digital currencies in everyday transactions.</p>
                     </div>
-                    <Link href="#" className='w-[70%] flex items-center justify-between gap-4 text-[72px]/[60px] font-sans font-normal tracking-tight text-primary border-b-2 border-primary rounded-sm mb-6'>Get Started <HiOutlineArrowRight/></Link>
+                    <Link href="#" className='w-[75%] flex items-center justify-between gap-4 text-[72px]/[60px] font-sans font-normal tracking-tight text-primary border-b-2 border-primary rounded-sm mb-6'>Get Started <HiOutlineArrowRight/></Link>
                 </div>
             </div>
         </>
