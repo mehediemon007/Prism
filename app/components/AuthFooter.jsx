dbb5b2334473a11ae8b94f986ca89c0570929319
mehediemon007 <mehediemon7@gmail.com>
2024-03-19 22:55:49 +0600

    fix

--- app/components/AuthFooter.jsx
+++ app/components/AuthFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AuthFooter = () => {
     return (
         <>
-            <div className="auth-footer">
+            <div className="auth-footer mt-[100px]">
                 <div className="flex">
                     <p>Copyright ©Prism 2024.</p>
                 </div>
