c3ccdfefea978d096570ad56cb8f5a9354651a3d
mehediemon007 <mehediemon7@gmail.com>
2024-03-20 04:06:30 +0600

    responsive

--- app/components/Features.jsx
+++ app/components/Features.jsx
@@ -11,12 +11,12 @@ const Features = () => {
     return (
         <>  
             <div className="features grid xl:grid-cols-2 gap-6 mt-6">
-                <div className="card border border-border bg-gradient">
+                <div className="card border border-border bg-gradient flex flex-col-reverse lg:flex-row xl:flex-col">
                     <div className="relative overflow-hidden pb-16">
-                        <Image src="/assets/imgs/help.svg" priority={true} width={0} height={0} sizes='100vw' className='w-[55%] m-6' alt='help'/>
+                        <Image src="/assets/imgs/help.svg" priority={true} width={0} height={0} sizes='100vw' className='w-[55%] ms-6 md:m-6' alt='help'/>
                         <Image src="/assets/imgs/ques.svg" priority={true} width={0} height={0} sizes="100vw" className='w-[44%] absolute top-[120px] right-[-40px]' alt='ques'/>
                     </div>
-                    <div className="card-content p-6 pt-0">
+                    <div className="card-content p-6 md:pt-0">
                         <div className='flex justify-between items-center'>
                             <h2>24/7 Customer Support</h2>
                             <PlusButton/>
@@ -24,7 +24,7 @@ const Features = () => {
                         <p>We offer round-the-clock customer service, ensuring you have continuous support and assistance for any issues or inquiries, no matter the time or day.</p>
                     </div>
                 </div>
-                <div className="card border border-border bg-gradient">
+                <div className="card border border-border bg-gradient flex flex-col lg:flex-row xl:flex-col">
                     <div className="card-content p-6">
                         <div className='flex justify-between items-center'>
                             <h2>Transparent Pricing</h2>
@@ -37,7 +37,7 @@ const Features = () => {
                         <Image src="/assets/imgs/map.svg" priority={true} width={0} height={0} sizes='100vw' className='w-full' alt='map'/>
                     </div>
                 </div>
-                <div className="card border border-border bg-gradient">
+                <div className="card border border-border bg-gradient flex flex-col lg:flex-row xl:flex-col">
                     <div className="card-content p-6">
                         <div className='flex justify-between items-center'>
                             <h2>TUnlimited Top-up and Spending</h2>
@@ -49,30 +49,32 @@ const Features = () => {
                         <Image src="/assets/imgs/outflow.svg" priority={true} width={0} height={0} sizes='100vw' className='w-full' alt='workflow'/>
                     </div>
                 </div>
-                <div className="card border border-border bg-gradient p-6 overflow-hidden">
-                    <div className="md:max-w-[80%] rounded-[16px]">
-                        <div className="client-info flex items-center gap-4 bg-[#2E2E2E] p-4 md:py-6 md:px-4 lg:py-8 lg:px-6 rounded-[16px]">
-                            <Image src="/assets/imgs/jassica.png" alt='Jassica' priority={true} width={58} height={58}/>
-                            <div className="details">
-                                <h3 className='text-sm md:text-base lg:text-[18px]/[32px] font-poppins font-semibold'>Sarah John</h3>
-                                <p className='text-xs md:text-sm lg:text-base font-poppins font-normal text-slate m-0'>CEO of Own Multi Company</p>
+                <div className="card border border-border bg-gradient p-6 overflow-hidden flex flex-col-reverse lg:flex-row xl:flex-col gap-y-6">
+                    <div>
+                        <div className="md:max-w-[80%] rounded-[16px]">
+                            <div className="client-info flex items-center gap-4 bg-[#2E2E2E] p-3 md:py-6 md:px-4 lg:py-8 lg:px-6 rounded-[16px]">
+                                <Image src="/assets/imgs/jassica.png" alt='Jassica' priority={true} width={58} height={58} className='w-auto h-auto'/>
+                                <div className="details">
+                                    <h3 className='text-sm md:text-base lg:text-[18px]/[32px] font-poppins font-semibold'>Sarah John</h3>
+                                    <p className='text-xs md:text-sm lg:text-base font-poppins font-normal text-slate m-0'>CEO of Own Multi Company</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="calculaotr">
-                        <div className='flex justify-end items-center gap-2 mt-8 mr-[-24px]'>
-                            <span className='inline-block px-1 py-0 rounded-sm bg-[#F9A80A] text-sm font-poppins font-semibold'>USD</span>
-                            <h2 className='w-[45%] text-6xl font-poppins font-normal border-b-2 border-border pb-2'><sup className='text-2xl top-[-32px]'>$</sup>560.00</h2>
-                        </div>
-                        <div className='flex gap-3 items-center justify-end mt-6'>
-                            <span className='text-2xl font-poppins font-normal text-[#D93C3C]'>+ Fee $100</span>
-                            <div className='space-x-3'>
-                                <span className='pill'>100</span>
-                                <span className='pill'>500</span>
-                                <span className='pill'>1,000</span>
+                        <div className="calculaotr">
+                            <div className='flex justify-end items-center gap-2 mt-4 md:mt-8 md:mr-[-24px]'>
+                                <span className='inline-block px-1 py-0 rounded-sm bg-[#F9A80A] text-sm font-poppins font-semibold'>USD</span>
+                                <h2 className='w-[60%] md:w-[45%] text-3xl xl:text-6xl font-poppins font-normal border-b-2 border-border pb-2'><sup className='text-2xl top-[-32px]'>$</sup>560.00</h2>
                             </div>
+                            <div className='flex gap-3 items-center justify-end mt-4 xl:mt-6'>
+                                <span className='text-xs xl:text-2xl font-poppins font-normal text-[#D93C3C]'>+ Fee $100</span>
+                                <div className='space-x-1 xl:space-x-3'>
+                                    <span className='pill'>100</span>
+                                    <span className='pill'>500</span>
+                                    <span className='pill'>1,000</span>
+                                </div>
+                            </div>
+                            <button type='submit' className='w-[80%] h-8 xl:h-[68px] rounded-[2px] xl:rounded-lg text-xs xl:text-lg font-poppins font-semibold bg-primary text-black my-4 xl:my-6'>Continue</button>
                         </div>
-                        <button type='submit' className='w-[80%] h-[68px] rounded-lg text-lg font-poppins font-semibold bg-primary text-black my-6'>Continue</button>
                     </div>
                     <div className="card-content">
                         <div className='flex justify-between items-center'>
--- app/elements/PlusButton.jsx
+++ app/elements/PlusButton.jsx
@@ -6,7 +6,7 @@ import { HiPlus } from "react-icons/hi";
 const PlusButton = () => {
     return (
         <>
-            <span className='inline-flex justify-center items-center w-[72px] h-[72px] rounded-full  bg-primary'><HiPlus size={24} className='text-white'/></span>
+            <span className='inline-flex justify-center min-w-8 min-h-8 md:w-14 md:h-14 items-center xl:w-[72px] xl:h-[72px] rounded-full  bg-primary'><HiPlus size={24} className='text-white'/></span>
         </>
     )
 }
