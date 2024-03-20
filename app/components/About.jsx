639641484e6e13b45b342627548835fba35bb37d
mehediemon007 <mehediemon7@gmail.com>
2024-03-20 07:15:47 +0600

    fix

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
                         <h2 className='text-2xl xl:text-[40px]/[50px] font-title font-bold tracking-wide'>Global Spending with Prism Card</h2>
                         <p className='text-base xl:text-2xl/[40px] font-title font-bold tracking-[1px] mt-4 xl:mt-6'>Your Prism Card lets you freely spend your cryptocurrency at any location and time across the globe, providing a seamless way to use digital currencies in everyday transactions.</p>
                     </div>
-                    <Link href="#" className='w-max xl:w-[75%] flex items-center justify-between gap-4 text-[20px]/[32px] xl:text-[72px]/[60px] font-sans font-normal tracking-tight text-primary border-b-2 border-primary rounded-sm mt-4 mb-3 xl:mt-0 xl:mb-6'>Get Started <HiOutlineArrowRight/></Link>
+                    <Link href="#" className='w-max xl:w-[75%] flex items-center justify-between gap-4 text-[20px]/[32px] xl:text-4xl 2xl:text-[72px]/[60px] font-sans font-normal tracking-tight text-primary border-b-2 border-primary rounded-sm mt-4 mb-3 xl:mt-0 xl:mb-6'>Get Started <HiOutlineArrowRight/></Link>
                 </div>
             </div>
         </>
